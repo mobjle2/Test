9173913fa3844819ce3992013475533626583e42
mobjle2 <lktrungnguyen@gmail.com>
2026-01-28 09:07:51 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,10 @@
+// data.js – PLACE FULL DATA HERE
+// Format:
+// { code:"KM8V8001JM-B813", vip:"VIP 8" },
+
+const data = [
+  {code:"KM8V8001JM-B813", vip:"VIP 8"},
+  {code:"KM8V9001JM-B813", vip:"VIP 8"},
+  {code:"KM8V7001JA-B813", vip:"VIP 8"},
+  {code:"KM8F800JM-B813", vip:"VIP 9"}
+];
