88fefdfc5b04d0080ee31e87d3a11c736e42753e
mobjle2 <lktrungnguyen@gmail.com>
2026-01-31 15:20:08 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,23 +1,22 @@
 const data = [
 
-/* ===== 16G ===== */
-{code:"KLMAG4FE4B-B002", vip:"VIP 1", mmc:"MMC1", norm:"KLMAG4FE4BB002", chip:"SAM SUNG"},
-{code:"H26M54002EMR", vip:"VIP 1", mmc:"MMC1", norm:"H26M54002EMR", chip:"SK hynix"},
+/* 16G */
+{code:"KLMAG4FE4B-B002", mmc:"MMC1", norm:"KLMAG4FE4BB002", chip:"SAM SUNG"},
+{code:"H26M54002EMR", mmc:"MMC1", norm:"H26M54002EMR", chip:"SK hynix"},
 
-/* ===== 32G ===== */
-{code:"KLMBG8FE4B-B001", vip:"VIP 2", mmc:"MMC2", norm:"KLMBG8FE4BB001", chip:"SAM SUNG"},
-{code:"SDINBDA4-32G", vip:"VIP 2", mmc:"MMC2", norm:"SDINBDA432G", chip:"SANDISK"},
+/* 32G */
+{code:"KLMBG8FE4B-B001", mmc:"MMC2", norm:"KLMBG8FE4BB001", chip:"SAM SUNG"},
 
-/* ===== 64G ===== */
-{code:"KLMCGAFE4B-B001", vip:"VIP 3", mmc:"MMC3", norm:"KLMCGAFE4BB001", chip:"SAM SUNG"},
+/* 64G */
+{code:"KLMCGAFE4B-B001", mmc:"MMC3", norm:"KLMCGAFE4BB001", chip:"SAM SUNG"},
 
-/* ===== 128G ===== */
-{code:"KLMDG8JENB-B041", vip:"VIP 4", mmc:"MMC4", norm:"KLMDG8JENBB041", chip:"SAM SUNG"},
+/* 128G */
+{code:"KLMDG8JENB-B041", mmc:"MMC4", norm:"KLMDG8JENBB041", chip:"SAM SUNG"},
 
-/* ===== 256G ===== */
-{code:"JZ369", vip:"VIP 5", mmc:"MMC5", norm:"JZ369", chip:"—"},
+/* 256G */
+{code:"JZ369", mmc:"MMC5", norm:"JZ369", chip:"MICRON"},
 
-/* ===== 512G ===== */
-{code:"KLUFGSRIEM-B0C1", vip:"VIP 6", mmc:"MMC6", norm:"KLUFGSRIEMB0C1", chip:"SAM SUNG"}
+/* 512G */
+{code:"KLUFGSRIEM-B0C1", mmc:"MMC6", norm:"KLUFGSRIEMB0C1", chip:"SAM SUNG"}
 
 ];
